298278a60f98f7ff5ccdc2ae6d513295162eed55
mustapha bousfina <bousfinamustapha@gmail.com>
2023-10-31 12:20:30 +0100

    normalizing paths

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -10,9 +10,19 @@ class Router
 
     public function add(string $method, string $path)
     {
+        $path = $this->normalizePath($path);
+
         $this->routes[] = [
             'path' => $path,
             'method' => strtoupper($method),
         ];
+    }
+
+    private function normalizePath(string $path): string
+    {
+        $path = trim($path, '/');
+        $path = "/{$path}/";
+
+        return $path;
     }
 }
