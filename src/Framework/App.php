69615fe4ca3f326fcf7f45cb5c8b4a05d35fe74c
mustapha bousfina <bousfinamustapha@gmail.com>
2023-10-31 11:42:55 +0100

    creating a Router class

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Router;
+
 class App
 {
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
     public function run()
     {
         echo "Application is running";
