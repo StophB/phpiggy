d6daca9a520e4a3c6223e559a40975df38a18d6e
mustapha bousfina <bousfinamustapha@gmail.com>
2023-10-31 13:04:12 +0100

    Dispatching a Route

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -28,4 +28,11 @@ class Router
 
         return $path;
     }
+
+    public function dispatch(string $path, string $method){
+        $path = $this->normalizePath($path);
+        $method = strtoupper($method);
+
+        echo $path . $method;
+    }
 }
