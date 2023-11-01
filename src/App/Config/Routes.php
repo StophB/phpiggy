74c589eec1590ec0722bf7a67863d834f57ee848
mustapha bousfina <bousfinamustapha@gmail.com>
2023-11-01 13:17:56 +0100

    Containers and Dependency Injection

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Controllers\HomeController;
-use App\Controllers\AboutController;
+use App\Controllers\{HomeController, AboutController};
 
 function registerRoutes(App $app)
 {
-    $app->get('/', [HomeController::class, 'home']);
-    $app->get('/about', [AboutController::class, 'about']);
+  $app->get('/', [HomeController::class, 'home']);
+  $app->get('/about', [AboutController::class, 'about']);
 }
